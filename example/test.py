c80b4a4fbd50760cc3b306cc6799b299f557b065
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-04-02 00:03:34 +0200

    Update example code
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- example/test.py
+++ example/test.py
@@ -1,15 +1,17 @@
-# Copyright (c) 2014, Patrick Uiterwijk <patrick@puiterwijk.org>
+# Written in 2014-2015, Patrick Uiterwijk <patrick@puiterwijk.org>
+# Written in 2015, Pierre-Yves Chibon <pingou@pingoured.fr>
 #
-# This file is part of JSAutoLogin.
+# This file is part of JSAutoLogin Example Code.
 # This example code is licensed under the CC0 license.
 #
-# To the extent possible under law, Patrick Uiterwijk
-# has waived all copyright and related or neighboring
-# rights to JSAutoLogin.
-# This work is published from: the Netherlands.
+# To the extent possible under law, the authors
+# have waived all copyright and related or neighboring
+# rights to JSAutoLogin Example Code.
+# This work is published from: the Netherlands, France.
+
+OPENID_ENDPOINT = 'http://localhost:5050/'
 
 import flask
-import jinja2
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -45,20 +47,20 @@ def login():
     error = oid.fetch_error()
     if error:
         flask.flash(error)
-        return flask.render_template('loggedin.html',
-                                     success='false',
+        return flask.render_template('login_error.html',
                                      error=error)
     else:
         immediate = False
         if flask.request.args.get('immediate', None) == 'true':
             immediate = True
-        return oid.try_login('http://localhost:5050/', ask_for=['email', 'nickname'],
-                                     ask_for_optional=['fullname'],
-                                     immediate=immediate)
+        return oid.try_login(OPENID_ENDPOINT,
+                             ask_for=['email', 'nickname'],
+                             ask_for_optional=['fullname'],
+                             immediate=immediate)
 
 
 @oid.after_login
-def create_or_login(resp):
+def after_login(resp):
     flask.session['loggedin'] = True
     flask.session['user'] = {'nickname': resp.nickname,
                              'fullname': resp.fullname,
@@ -66,6 +68,5 @@ def create_or_login(resp):
     return flask.redirect('/')
 
 
-
 APP.debug = True
-APP.run(port=6060,debug=True)
+APP.run(port=6060, debug=True)
